92596565012471aa5526a8be082ab75238d102ed
Alex Stormwood <alex.holder@bigfootds.com>
2024-12-02 15:56:28 +1100

    feat!: initial release functionality

--- README.md
+++ README.md
@@ -1,6 +1,62 @@
-# Bigfoot-Fetcher
+# @bigfootds/Bigfoot-Fetcher
+
 JavaScript fetch override for cohesive, consistent usage across various BigfootDS projects.
+
+Note that this package, while publicly available and MIT-licensed, is **not** intended for public contribution/collaboration. We just need an easy way to consolidate some functionality that will be reused across multiple projects, and an NPM package is a nice, easy way to do that.
+
+## Installation
+
+`npm install @bigfootds/bigfoot-fetcher`
+
+## Usage
+
+Import the package as:
+
+```js
+const bigfootFetcher = require("@bigfootds/bigfoot-fetcher");
+```
+
+Or:
+
+```js
+import bigfootFetcher from "@bigfootds/bigfoot-fetcher";
+```
+
+
+Then, use `bigfootFetcher` in place of any fetch function calls. It's a wrapper around fetch!
+
+## More Info
 
 These headers are added to all fetch requests that use this package's fetcher function:
 
-- 
+
+```typescript
+interface BigfootDSConfig {
+    // Set in environment variables or package.json, access with properties like `process.env.npm_package_config_bigfootds_productName`:
+	productName?: string // also retrievable as environment variable
+
+    // Retrievable via Bowser or "My User Agent Parser" NPM packages, or environment variables:
+    browserName?: string,
+    browserVersion?: string,
+    browserEngineName?: string,
+    browserEngineVersion?: string,
+    osName?: string,
+    osVersion?: string,
+    osVersionName?: string,
+    platformType?: string, // also retrievable as environment variable
+
+    // Retrievable as environment variables:
+    platformName?: string // set as environment variable, preferably at build time in a GitHub Action workflow
+}
+```
+
+The `package.json` file of a project using this package should include a config object like this:
+
+```json
+"config": {
+    "bigfootds": {
+        "productName": "Game Name Goes Here",
+        "platformName": "Steam Deck or other platform determined at build time"
+    }
+},
+```
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,145 @@
+import dotenv from "dotenv";
+dotenv.config();
+import Bowser from "bowser"; // TypeScript
+import MyUaParser from "my-ua-parser";
+import os from "node:os";
 
-console.log(process.env.npm_package_config_bigfootds_game);
-console.log(process.env.npm_package_testo);
-console.log(process.env.npm_package_scripts_start)
+// Good:
+// console.log(process.env.npm_package_config_bigfootds_game);
+// Junk:
+// console.log(process.env.npm_package_bigfootds_game);
+// console.log(process.env.npm_package_testo);
+// console.log(process.env.npm_package_scripts_start)
 
 export default function fetcher(requestTarget: RequestInfo | URL, options: RequestInit){
 	const defaultHeaders: HeadersInit = new Headers(options.headers);
-	defaultHeaders.set("browserName", "")
+    let bigfootDSConfigData: BigfootDSConfig = {}
+    if (typeof self === 'undefined') { 
+        /* neither web window nor web worker, must be node environment */ 
+        bigfootDSConfigData = getInfoViaNode();
+    } else {
+        // global "window" object is available, must be a browser
+        bigfootDSConfigData = getInfoViaBrowser();
+    }
+
+
+    (Object.keys(bigfootDSConfigData) as Array<keyof BigfootDSConfig>).forEach((key) => {
+        if (bigfootDSConfigData[key]){
+            defaultHeaders.set(key, bigfootDSConfigData[key]);
+        }
+    });
+
+
 	return fetch(requestTarget);
 }
 
-function updateOptions(options: RequestInit) {
-	const updated: BigfootFetchOptions = {...options};
-	if (localStorage.jwt) {
-		updated.headers = {
-		  ...updated.headers,
-		  Authorization: `Bearer ${localStorage.jwt}`,
-		};
-	  }
-	return updated;
+export function getInfoViaBrowser(){
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    const bowserResult = browser.getResult();
+    const userAgentParsed = MyUaParser(window.navigator.userAgent);
+
+    let result: BigfootDSConfig = {
+        browserEngineName: 
+            bowserResult.engine.name ||
+            userAgentParsed.engine.name,
+        browserEngineVersion: 
+            bowserResult.engine.version || 
+            userAgentParsed.engine.version,
+        browserName: 
+            bowserResult.browser.name || 
+            userAgentParsed.browser.name,
+        browserVersion: 
+            bowserResult.browser.version ||
+            userAgentParsed.browser.version,
+        osName: 
+            bowserResult.os.name ||
+            userAgentParsed.os.name,
+        osVersion: 
+            bowserResult.os.version  || 
+            userAgentParsed.os.version,
+        osVersionName: 
+            bowserResult.os.versionName,
+        platformType: 
+            process.env.PLATFORMTYPE || 
+            process.env.PLATFORM_TYPE || 
+            process.env.REACT_APP_PLATFORM_TYPE || 
+            process.env.REACT_APP_PLATFORMTYPE ||
+            process.env.VITE_PLATFORM_TYPE || 
+            process.env.VITE_PLATFORMTYPE ||
+            bowserResult.platform.type,
+        platformName:
+            process.env.PLATFORMNAME || 
+            process.env.PLATFORM_NAME || 
+            process.env.REACT_APP_PLATFORM_NAME || 
+            process.env.REACT_APP_PLATFORMNAME ||
+            process.env.VITE_PLATFORM_NAME || 
+            process.env.VITE_PLATFORMNAME,
+        productName:
+            process.env.npm_package_config_bigfootds_productName ||
+            process.env.npm_package_productName ||
+            process.env.PRODUCTNAME || 
+            process.env.PRODUCT_NAME || 
+            process.env.REACT_APP_PRODUCT_NAME || 
+            process.env.REACT_APP_PRODUCTNAME ||
+            process.env.VITE_PRODUCT_NAME || 
+            process.env.VITE_PRODUCTNAME,
+    }
+
+    return result;
 }
 
-interface BigfootFetchOptions extends Omit<RequestInit, "headers"> {
-	headers?: any;
+export function getInfoViaNode(){
+
+    let result: BigfootDSConfig = {
+        osName: 
+            os.type(),
+        osVersion: 
+            os.release(),
+        platformType: 
+            process.env.PLATFORMTYPE || 
+            process.env.PLATFORM_TYPE || 
+            process.env.REACT_APP_PLATFORM_TYPE || 
+            process.env.REACT_APP_PLATFORMTYPE ||
+            process.env.VITE_PLATFORM_TYPE || 
+            process.env.VITE_PLATFORMTYPE,
+        platformName:
+            process.env.PLATFORMNAME || 
+            process.env.PLATFORM_NAME || 
+            process.env.REACT_APP_PLATFORM_NAME || 
+            process.env.REACT_APP_PLATFORMNAME ||
+            process.env.VITE_PLATFORM_NAME || 
+            process.env.VITE_PLATFORMNAME,
+        productName:
+            process.env.npm_package_config_bigfootds_productName ||
+            process.env.npm_package_productName ||
+            process.env.PRODUCTNAME || 
+            process.env.PRODUCT_NAME || 
+            process.env.REACT_APP_PRODUCT_NAME || 
+            process.env.REACT_APP_PRODUCTNAME ||
+            process.env.VITE_PRODUCT_NAME || 
+            process.env.VITE_PRODUCTNAME,
+    }
+
+    return result;
+}
+
+
+interface BigfootDSConfig {
+    // Set in package.json, access with properties like `process.env.npm_package_config_bigfootds_productName`:
+	productName?: string // also retrievable as environment variable
+
+    // Retrievable via Bowser NPM package:
+    browserName?: string,
+    browserVersion?: string,
+    browserEngineName?: string,
+    browserEngineVersion?: string,
+    osName?: string,
+    osVersion?: string,
+    osVersionName?: string,
+    platformType?: string, // also retrievable as environment variable
+
+    // Retrievable as environment variables:
+    platformName?: string // set as environment variable, preferably at build time in a GitHub Action workflow
 }
 
 // In a browser with a correct meta HTML tag, this natively gets some device info: 
@@ -71,3 +189,6 @@ interface BigfootFetchOptions extends Omit<RequestInit, "headers"> {
 
 // ElectronJS can set a user agent like so:
 // https://stackoverflow.com/questions/35672602/how-to-set-electron-useragent
+
+// Set environment variables in GitHub Actions workflows like so:
+// https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables
